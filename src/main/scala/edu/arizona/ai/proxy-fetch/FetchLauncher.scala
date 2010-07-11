3c9b3abbc00b3fb791c226ed00430e88f999641c
Ximing Yu <ximing.yu@gmail.com>
2010-07-10 22:13:43 -0700

    change package structure

---
+++ src/main/scala/edu/arizona/ai/proxy-fetch/FetchLauncher.scala
@@ -0,0 +1,96 @@
+package edu.arizona.ai
+
+import scala.actors.Actor._
+import collection.mutable.ListBuffer
+import org.slf4j.LoggerFactory
+import edu.arizona.ai.proxyfetch.Utility
+import java.util.{Properties, Date}
+import java.io.{IOException, BufferedReader, InputStreamReader, FileInputStream}
+
+/**
+ * @author Ximing Yu
+ * @version 0.2, 7/5/2010
+ */
+
+object FetchLauncher extends Connector with Logging {
+  var startTime: Long = 0
+  var spideringTime: Long = 0
+  val sites = new ListBuffer[String]
+  val caller = self
+
+  def main(args: Array[String]) {
+    launchCrawling(doTesting = true, firstTime = true)
+  }
+
+  def launchCrawling(doTesting: Boolean = false, firstTime: Boolean = false) = {
+    startTime = System.currentTimeMillis
+
+    if (firstTime)
+      initDBTable
+
+    val is = new FileInputStream(ClassLoader.getSystemResource(PropertyLoader.proxySiteListFile).getFile)
+    Utility.convertStreamToString(is).split("\n") foreach {x=> sites += x.trim}
+
+    val starter = actor {
+      var count = 0
+      loopWhile(count < sites.size) {
+        receive {
+          case "Finished" => count += 1; log.info("One site spidering finished, count=" + count)
+          case _ => log.warn("Unknown message")
+        }
+        if (count == sites.size) {
+          caller ! "Spider Finished"
+        }
+
+      }
+    }
+
+    val fetcher = new FetcherActor
+    fetcher.start
+    for (site <- sites) fetcher ! Pair(starter, site)
+
+    receive {
+      case "Spider Finished" =>
+        spideringTime = System.currentTimeMillis - startTime
+        log.info("Spider finished in " + spideringTime / 1000 + " seconds")
+        log.info(getStoredProxies.size + " proxies spidered in total.")
+        if (doTesting) {
+          log.info("To start testing now.")
+          launchTesting
+        }
+      case _ => log.warn("Unknown message")
+    }
+  }
+
+  def launchTesting: Unit = {
+    val numOfBatches = PropertyLoader.numOfTestingThread
+    val testingStartTime = System.currentTimeMillis
+    log.info("Proxy testing started in parallel. " + numOfBatches + " threads used.")
+    val testingStarter = actor {
+      var count = 0
+      loopWhile(count < numOfBatches) {
+        receive {
+          case "Finished" => count += 1; log.info("One batch of testing finished, count=" + count)
+          case "Stop" => count = numOfBatches
+          case _ => log.warn("Unknown message")
+        }
+        if (count == numOfBatches) {
+          log.info("Batch testing finished")
+          log.info("Testing: " + (System.currentTimeMillis - testingStartTime) / 1000 + " seconds")
+          val userChoice = readLine()
+          Thread.sleep(5000)
+          log.info("To start testing another batch")
+          launchTesting
+        }
+      }
+    }
+
+    val allProxies = getUntestedProxiesByBatch
+    if (allProxies.size == 0) testingStarter ! "Stop"
+    val batchSize = allProxies.size / numOfBatches + 1
+    val testingActor = new TestingActor
+    testingActor.start
+    (1 to numOfBatches) foreach {i => testingActor ! Pair(testingStarter, allProxies.slice((i - 1) * batchSize, (i * batchSize) min allProxies.size))}
+  }
+
+}
---
+++ src/main/scala/edu/arizona/ai/proxyfetch/Utility.scala
@@ -0,0 +1,177 @@
+package edu.arizona.ai.proxyfetch
+
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.DefaultHttpClient
+import java.io.{InputStream, BufferedReader, InputStreamReader, IOException}
+import org.apache.http.conn.params.ConnRoutePNames
+import org.apache.http.HttpHost
+import java.security.MessageDigest
+import java.sql.Connection
+import java.text.SimpleDateFormat
+import java.util.Date
+import org.slf4j.LoggerFactory
+import org.apache.http.params.{CoreConnectionPNames, CoreProtocolPNames}
+import edu.arizona.ai.{PropertyLoader, Logging, Connector, Proxy}
+
+/**
+ * @auhtor Ximing Yu
+ * @version 0.2, 7/5/2010
+ */
+
+object Utility extends Connector with Logging {
+
+  val ipPortRE          =   """\b(?:\d{1,3}\.){3}\d{1,3}:\d{2,6}\b""".r
+  val domainNamePortRE  =   """\b[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}:\d{2,6}\b""".r
+  val addressPortRE     =   """(.+?):(.+?)""".r
+
+  val cnproxyAssignRE   =   """(\w)="(\d)";""".r
+  val cnproxyRowRE      =   """((?:\d{1,3}\.){3}\d{1,3})<SCRIPT[^>]+>document\.write\(":"(.+?)\)""".r
+  val samairAssignRE    =   """(\w)=(\d);""".r
+  val samairRowRE       =   """((?:\d{1,3}\.){3}\d{1,3})<script[^>]+>document\.write\(":"(.+?)\)""".r
+  val riTableRE        =   """(?is)var x=Math\.round\(Math\.sqrt\((\d+)\).+?hideText\d*\('(.+?)'\)""".r
+  val proxyCnIpPortRE   =   """(?is)onDblClick="clip\('((?:\d{1,3}\.){3}\d{1,3}:\d{2,6})'\);alert""".r
+  
+  val cnproxyeduRE      =   """(.+?cnproxy.+?edu.+?)""".r
+  val cnproxyRE         =   """(.+?cnproxy.+?)""".r
+  val freshproxyRE      =   """(.+?freshproxy.+?)""".r
+  val myproxyRE         =   """(.+?my-proxy.+?)""".r
+  val proxylistRE       =   """(.+?proxylist.+?)""".r
+  val samairRE          =   """(.+?samair.+?)""".r
+  val rosinstrumentRE   =   """(.+?rosinstrument.+?)""".r
+  val proxycnRE         =   """(.+?proxycn.+?)""".r
+
+  def testProxy(proxy: Proxy) {
+    log.info("Testing proxy {}:{}", proxy.server, proxy.port)
+    val beforeTest = System.currentTimeMillis
+    var pageContent = ""
+    try {
+      pageContent = Utility.getWebContentWithProxy(PropertyLoader.testUrl, proxy.server, proxy.port).getOrElse("")
+    } catch {
+      case e: Exception => log.error("Proxy {}:{} does not work", proxy.server, proxy.port)
+    }
+    val md5WithProxy = Utility.md5SumString(pageContent.getBytes)
+    val span = (System.currentTimeMillis - beforeTest) / 1000
+    if (span > 10) {
+      deleteProxy(proxy.server, proxy.port)
+      log.warn("Proxy {}:{} will be discarded::response too slow", proxy.server, proxy.port)
+      throw new Exception("Proxy not working")
+    } else {
+      val md5WithoutProxy = Utility.md5SumString(Utility.getWebContent(PropertyLoader.testUrl).getOrElse("").getBytes)
+      if (md5WithProxy == md5WithoutProxy) {
+        val newProxy = new Proxy(proxy.server, proxy.port, (System.currentTimeMillis - beforeTest) / 1000.0, proxy.errorTime, true)
+        log.info("Proxy {}:{} responded within required time period.", proxy.server, proxy.port)
+        updateProxy(newProxy)
+      } else {
+        log.warn("Proxy {}:{} will be discarded::cannot get the test page", proxy.server, proxy.port)
+        deleteProxy(proxy.server, proxy.port)
+        throw new Exception("Proxy not working")
+      }
+    }
+  }
+
+  def persistProxyList(proxyList: List[Proxy]) = {
+    proxyList foreach {storeProxy(_)}
+  }
+
+  def getHttpClient: HttpClient = {
+    val client: HttpClient = new DefaultHttpClient()
+    client.getParams.setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.1 (KHTML, like Gecko) Chrome/6.0.437.3 Safari/534.1")
+    client.getParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, PropertyLoader.responseLimit.toInt * 1000)
+    client.getParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, PropertyLoader.responseLimit.toInt * 1000)
+    client
+  }
+
+  def getHttpClient(server: String, port: Int): HttpClient = {
+    val client: HttpClient = new DefaultHttpClient()
+    client.getParams.setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.1 (KHTML, like Gecko) Chrome/6.0.437.3 Safari/534.1")
+    client.getParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, PropertyLoader.responseLimit.toInt * 1000)
+    client.getParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, PropertyLoader.responseLimit.toInt * 1000)
+    client.getParams.setParameter(ConnRoutePNames.DEFAULT_PROXY, new HttpHost(server, port))
+    client
+  }
+
+  def getWebContent(url: String): Option[String] = {
+    val client = getHttpClient
+    try {
+      val response = client.execute(new HttpGet(url))
+      if (response.getStatusLine.getStatusCode != 200) throw new Exception("Server response: " + response.getStatusLine.toString)
+      val entity = response.getEntity
+      return new Some(convertStreamToString(entity.getContent))
+    } catch {
+      case e: Exception => println("[ERROR] " + e.getMessage); return Some("")
+    } finally {
+      client.getConnectionManager.shutdown
+    }
+  }
+
+  def getWebContentWithProxy(url: String, server: String, port: Int): Option[String] = {
+    val client = getHttpClient(server, port)
+    try {
+      val response = client.execute(new HttpGet(url))
+      if (response.getStatusLine.getStatusCode != 200) throw new Exception("Server response: " + response.getStatusLine.toString)
+      val entity = response.getEntity
+      return new Some(convertStreamToString(entity.getContent))
+    } catch {
+      case e: Exception => println("[ERROR] " + e.getMessage); return Some("")
+    } finally {
+      client.getConnectionManager.shutdown
+    }
+  }
+
+  def md5SumString(bytes : Array[Byte]) : String = {
+    val md5 = MessageDigest.getInstance("MD5")
+    md5.reset()
+    md5.update(bytes)
+
+    md5.digest().map(0xFF & _).map { "%02x".format(_) }.foldLeft(""){_ + _}
+  }
+
+  def convertStreamToString(is : InputStream) : String = {
+    def inner(reader : BufferedReader, sb : StringBuilder) : String = {
+      val line = reader.readLine()
+      if(line != null) {
+        try {
+          inner(reader, sb.append(line + "\n"))
+        } catch {
+          case e: IOException => e.printStackTrace()
+        } finally {
+          try {
+            is.close()
+          } catch {
+            case e: IOException => e.printStackTrace()
+          }
+        }
+      }
+      sb.toString()
+    }
+
+    inner(new BufferedReader(new InputStreamReader(is)), new StringBuilder())
+  }
+
+  def unescapeString(text: String, param: Int): String = {
+		val pattern = """(?is)(%\w{2})""".r
+		var toText = text
+    pattern.findAllIn(text).foreach({x => toText = toText.replace(x, decodeHexString(x.substring(1,3)))})
+		val sb = new StringBuilder
+		for (i <- 0 until toText.length) {
+			val newChar = (toText.charAt(i) ^ param).asInstanceOf[Char]
+			sb.append(newChar)
+		}
+		sb.toString()
+	}
+
+  def decodeHexString(hexText: String): String = {
+    if (hexText.length > 0) {
+      val numBytes = hexText.length / 2
+      var rawToByte = new Array[Byte](numBytes)
+      var offset = 0
+      for (i <- 0 until numBytes) {
+        val chunk = hexText.substring(offset, offset + 2)
+        offset += 2
+        rawToByte(i) = (Integer.parseInt(chunk, 16) & 0x000000FF).asInstanceOf[Byte]
+      }
+      new String(rawToByte)
+    } else {""}
+  }
+}
