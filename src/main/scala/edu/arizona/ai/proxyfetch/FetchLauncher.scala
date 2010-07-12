47379bac4a7f71e1ea797cf7aa07d7e115464c56
Ximing Yu <ximyu@ximyu-desktop.(none)>
2010-07-12 11:10:16 -0700

    Add exit condition

--- src/main/scala/edu/arizona/ai/proxyfetch/FetchLauncher.scala
+++ src/main/scala/edu/arizona/ai/proxyfetch/FetchLauncher.scala
@@ -70,22 +70,23 @@ object FetchLauncher extends Connector with Logging {
       loopWhile(count < numOfBatches) {
         receive {
           case "Finished" => count += 1; log.info("One batch of testing finished, count=" + count)
-          case "Stop" => count = numOfBatches
           case _ => log.warn("Unknown message")
         }
         if (count == numOfBatches) {
           log.info("Batch testing finished")
           log.info("Testing: " + (System.currentTimeMillis - testingStartTime) / 1000 + " seconds")
-          val userChoice = readLine()
-          Thread.sleep(5000)
-          log.info("To start testing another batch")
-          launchTesting
+          if (getUntestedProxiesByBatch.size > 0) {
+            Thread.sleep(5000)
+            log.info("To start testing another batch")
+            launchTesting
+          } else {
+            log.info("All testing finished.")
+          }
         }
       }
     }
 
     val allProxies = getUntestedProxiesByBatch
-    if (allProxies.size == 0) testingStarter ! "Stop"
     val batchSize = allProxies.size / numOfBatches + 1
     val testingActor = new TestingActor
     testingActor.start
